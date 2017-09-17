46c4c02021b76df1c76d92dab4e774bf5413d684
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-09-17 15:13:17 +0100

    added styling and images

--- main.js
+++ main.js
@@ -4,26 +4,23 @@ $(function(){
 
 	$("button").click(function() {
     $(".background").toggle();
+    $('x');
 });
 	$(document).ready(function(){
 	var app = {
 		cards: 
-		[1,1,
-		2,2,
-		3,3,
-		4,4,
-		5,5,
-		6,6,
-		7,7,], //array of cards 
+		[0,0,1,1,2,2,3,3,4,4,5,5,6,6,],
+		 //array of cards 
+		
 		init: function () {
-			app.shuffle()
+			app.shuffle();
 
 
 		},
 		shuffle: function() { //card shuffling function
 			var random = 0;
 			var temp = 0;
-			for(i =1; i < app.cards.length; i++) { //iterates through the number of cards 14 times and each time it will present a random number
+			for(i = 0; i < app.cards.length; i++) { //iterates through the number of cards 14 times and each time it will present a random number
 				random = Math.round(Math.random() * i);
 				console.log(random); //shows random 14 numbers in the console log
 				temp = app.cards[i];
